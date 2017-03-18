f58ebc3f68f97517d0d8b3694583239802b2b1f4
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-19 03:26:40 +0700

    Done exactly

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,37 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',
+    {
+      title: 'API Client Server Scoping',
+      newTitle: 'This is new title'
+    }
+  );
 });
 
+router.get('/skills', function(req, res, next) {
+  res.render('skills',
+    {
+      title: 'Skills',
+      newTitle: req.query.newtitle
+    }
+  );
+});
+
+router.get('/meals', function(req, res, next) {
+  res.render('meals',
+    {
+      title: 'Meals',
+    }
+  );
+});
+
+// router.get('/skills', function(req, res, next) {
+//   res.render('meals',
+//     {
+//       title: 'Skills',
+//       newTitle: req.query.newtitle
+//     }
+//   );
+// });
 module.exports = router;
